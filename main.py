6191a90c6ab0a5f405ae8ea8f642dc3f0334d670
IngLucaC <caccialiluca@gmail.com>
2025-10-04 21:47:48 +0200

    Optional Task1 added including the time module and charts

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import powerCurve as pc
+import time
 
 # This script computes and plots the power curve of a wind turbine
 
 # options (0 = linear, 1 = cubic)
-interpFlag = 1
+interpFlag = 0
 
 # fluid properties
 rho = 1.225  # Kg/m^3
@@ -24,19 +25,42 @@ U_cout = 25
 if interpFlag not in (0, 1):
     raise ValueError("interpFlag must be 0 (linear) or 1 (cubic)")
 
+# addition of python time module to measure the execution time of the (NON-VECTORIZED) \
+# power curve function
+# Note: time.time() was giving inconsistent results on my PC, so I used time.perf_counter()
+timeStart = time.perf_counter()
+P1 = pc.nonVectorized(P_rated, U, U_rated, U_cin, U_cout, interpFlag)
+timeEnd = time.perf_counter()
+dt1 = timeEnd - timeStart
+print(f"The execution time is{dt1:.10f} seconds")
 
-print(f"Power at U_rated = {P_rated/1e6:.1f} MW")
-
-# call for the power curve function
-P = pc.powerCurve(P_rated, U, U_rated, U_cin, U_cout, interpFlag)
+# addition of python time module to measure the execution time of the (VECTORIZED) \
+# power curve function
+# Note: time.time() was giving inconsistent results on my PC, so I used time.perf_counter()
+timeStart = time.perf_counter()
+P2 = pc.vectorized(P_rated, U, U_rated, U_cin, U_cout, interpFlag)
+timeEnd = time.perf_counter()
+dt2 = timeEnd - timeStart
+print(f"The execution time is{dt2:.10f} seconds")
 
 # power curve chart
 plt.figure(figsize=(8, 5))
-plt.plot(U, P / 1e6, label="Power Curve", color="blue")  # converti in MW
+
+plt.subplot(2, 1, 1)
+plt.plot(U, P1 / 1e6, label="Power Curve", color="blue")
 plt.xlabel("Wind speed [m/s]")
 plt.ylabel("Power [MW]")
-plt.title("Wind Turbine Power Curve")
+plt.title("Power Curve (built without vectorization)")
 plt.grid(True)
 plt.legend()
+
+plt.subplot(2, 1, 2)
+plt.plot(U, P2 / 1e6, label="Power Curve", color="blue")
+plt.xlabel("Wind speed [m/s]")
+plt.ylabel("Power [MW]")
+plt.title("Power Curve (built with vectorization)")
+plt.grid(True)
+plt.legend()
+
 plt.tight_layout()
 plt.show()
--- powerCurve.py
+++ powerCurve.py
@@ -1,7 +1,8 @@
-def powerCurve(P_rated, U, U_rated, U_cin, U_cout, interpFlag):
+def nonVectorized(P_rated, U, U_rated, U_cin, U_cout, interpFlag):
     """This function computes the power curve of a wind turbine"""
 
     import numpy as np
+    import matplotlib.pyplot as plt
 
     # check for the interpFlag type
     if interpFlag == 0:
@@ -27,4 +28,35 @@ def powerCurve(P_rated, U, U_rated, U_cin, U_cout, interpFlag):
             P[i] = P_rated
         else:
             P[i] = 0
+
+    return P
+
+
+def vectorized(P_rated, U, U_rated, U_cin, U_cout, interpFlag):
+    """This function computed the power curve using the vectorization"""
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    # check for the interpFlag type
+    if interpFlag == 0:
+
+        # linear interpolation
+        g = (U - U_cin) / (U_rated - U_cin)
+
+    else:
+
+        # cubic interpolation
+        g = U**3 / U_rated**3
+
+    # initialize the power array
+    P = np.zeros(len(U))
+
+    # compute the power curve
+    P = np.where(
+        U < U_cin,
+        0,
+        np.where(U < U_rated, g * P_rated, np.where(U < U_cout, P_rated, 0)),
+    )
+
     return P
